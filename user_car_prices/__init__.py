cd310b2bb34008904c6d75f25d4fca35f356820d
Harsh Agarwal <harshag.code@gmail.com>
2024-10-03 09:21:00 +0530

    version-setup

--- user_car_prices/__init__.py
+++ user_car_prices/__init__.py
@@ -2,4 +2,4 @@ import os
 from user_car_prices.config import config
 
 with open(os.path.join(config.PACKAGE_ROOT, 'VERSION')) as f:
-    config.__version__ = f.read().strip()
+    __version__ = f.read().strip()
